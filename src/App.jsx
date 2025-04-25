623bfe8abf37c136a087ee31ea73425efcd45c24
saiimonn <simongementiza@Simons-Laptop.local>
2025-04-26 01:28:15 +0800

    Added pics for gridsection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Navbar from './Navbar.jsx'
 import Iphone from './Iphone.jsx'
 import Watch from './Watch.jsx'
+import MothersDay from './MothersDay.jsx'
 
 function App() {
   const[showRegionHeader, setRegionHeader] = useState(true);
@@ -11,6 +12,7 @@ function App() {
         <Navbar showRegionHeader={showRegionHeader} setRegionHeader={setRegionHeader} />
         <Iphone showRegionHeader={showRegionHeader} />
         <Watch></Watch>
+        <MothersDay></MothersDay>
     </div>
   );
 }
---
+++ src/MothersDay.jsx
@@ -0,0 +1,10 @@
+
+function MothersDay() {
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default MothersDay
