3b61727aa825b257f646f246e03665b1bd3007b9
saiimonn <gementizasgg08@gmail.com>
2025-05-04 22:34:21 +0800

    finished v2 of carousel through separate data module

--- src/TvCarousel.jsx
+++ src/TvCarousel.jsx
@@ -1,82 +1,53 @@
 import { useState } from "react";
+import slides from './data/TVCarouselSlides.js';
 
-import baseball from './assets/CarouselImages/baseball.jpeg';
-import careme from './assets/CarouselImages/careme.jpeg';
-import dThief from './assets/CarouselImages/dopeThief.jpg';
-import friends from './assets/CarouselImages/friendsNeighbors.jpg';
-import govCheese from './assets/CarouselImages/govCheese.jpg';
-import season from './assets/CarouselImages/seasonPass.jpg';
-import severance from './assets/CarouselImages/severance.jpg';
-import shrinking from './assets/CarouselImages/shrinking.jpeg';
-import ted from './assets/CarouselImages/tedLasso.jpeg';
-import studio from './assets/CarouselImages/theStudio.jpeg';
-
-const images = [
-  friends,
-  studio,
-  careme,
-  severance,
-  season,
-  dThief,
-  ted,
-  baseball,
-  govCheese,
-  shrinking,
-];
-
-function TVCarousel() {
+export default function TVCarousel() {
 
   const [current, setCurrent] = useState(0);
 
   const nextSlide = () => {
-    setCurrent((prev) => (prev + 1) % images.length);
+    setCurrent((prev) => (prev + 1) % slides.length);
   };
 
   const prevSlide = () => {
-    setCurrent((prev) => (prev - 1 + images.length) % images.length);
+    setCurrent((prev) => (prev - 1 + slides.length) % slides.length);
   };
 
   return (
     <>
-        <div className="relative w-full overflow-hidden h-170 mt-3">
-            <div
-                className="flex transition-transform duration-500 ml-14 mr-38 gap-4"
-                style={{ transform: `translateX(-${current * 100}%)` }}>
-                {images.map((src, idx) => (
-                <img
-                    key={idx}
-                    src={src}
-                    className="min-w-full h-full object-cover"
-                    alt={`Slide ${idx + 1}`}
-                />
-                ))}
+      <div className="relative w-full overflow-hidden h-170 mt-3">
+        <div className="flex transition-transform duration-500 ml-14 mr-38 gap-4" style={{ transform: `translateX(-${current * 100}%)` }}>
+          {slides.map((slide, idx) => (
+
+            <div key = {idx} className = "min-w-full h-full relative">
+              <img src={slide.image} alt={`Slide ${idx + 1}`} className = "w-full h-full object-cover" />
+              <div className = "absolute inset-0 pl-20 py-20 flex flex-row justify-start items-end p-8 bg-gradient-to-r from-black/50 to-transparent text-white gap-4">
+                <button class = "cursor-pointer px-5 py-2 bg-white text-black rounded-full w-fit">{slide.buttonCaption}</button>
+                <p className = "text-xl font-light mb-1"><span class = "font-semibold">{slide.genre}</span> {slide.caption}</p>
+              </div>
             </div>
 
-            <button
-                onClick={prevSlide}
-                className="absolute top-1/2 left-4 -translate-y-1/2 bg-white bg-opacity-80 px-3 py-1 rounded-full shadow"
-            >
-                ‹
-            </button>
-            <button
-                onClick={nextSlide}
-                className="absolute top-1/2 right-4 -translate-y-1/2 bg-white bg-opacity-80 px-3 py-1 rounded-full shadow"
-            >
-                ›
-            </button>
-
-            <div className="absolute bottom-4 w-full flex justify-center gap-2">
-                {images.map((_, idx) => (
-                <button
-                    key={idx}
-                    onClick={() => setCurrent(idx)}
-                    className={`w-3 h-3 rounded-full ${current === idx ? "bg-black" : "bg-gray-300"}`}
-                />
-                ))}
-            </div>
+              ))}
         </div>
+
+        <button onClick={prevSlide} className="absolute top-1/2 left-4 -translate-y-1/2 bg-white bg-opacity-80 px-3 py-1 rounded-full shadow">
+            ‹
+        </button>
+
+        <button onClick={nextSlide} className="absolute top-1/2 right-4 -translate-y-1/2 bg-white bg-opacity-80 px-3 py-1 rounded-full shadow">
+            ›
+        </button>
+
+        <div className="absolute bottom-4 w-full flex justify-center gap-2">
+            {slides.map((_, idx) => (
+            <button
+                key={idx}
+                onClick={() => setCurrent(idx)}
+                className={`w-3 h-3 rounded-full ${current === idx ? "bg-black" : "bg-gray-300"}`}
+            />
+            ))}
+        </div>
+      </div>
     </>
   );
 }
-
-export default TVCarousel
---
+++ src/data/TVCarouselSlides.js
@@ -0,0 +1,84 @@
+import baseball from '../assets/CarouselImages/baseball.jpeg';
+import careme from '../assets/CarouselImages/careme.jpeg';
+import dThief from '../assets/CarouselImages/dopeThief.jpg';
+import friends from '../assets/CarouselImages/friendsNeighbors.jpg';
+import govCheese from '../assets/CarouselImages/govCheese.jpg';
+import season from '../assets/CarouselImages/seasonPass.jpg';
+import severance from '../assets/CarouselImages/severance.jpg';
+import shrinking from '../assets/CarouselImages/shrinking.jpeg';
+import ted from '../assets/CarouselImages/tedLasso.jpeg';
+import studio from '../assets/CarouselImages/theStudio.jpeg';
+
+const slides = [
+  {
+    image: friends,
+    genre: "Drama •",
+    caption: "Live your best Lie.",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: studio,
+    genre: "Comedy •",
+    caption: "Seth Rogen dives into the wild chaos of Hollywood in this all-start satire.",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: careme,
+    genre: "Drama •",
+    caption: "A brilliant, seductive chef turns to espionage in Napoleon-era France.",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: severance,
+    genre: "Thriller •",
+    caption: "There's more to work than life.",
+    buttonCaption: "Stream now"
+  }, 
+  
+  {
+    image: season,
+    genre: "",
+    caption: "Watch every club, every match, live—all season long.",
+    buttonCaption: "See the schedule"
+  },
+
+  {
+    image: dThief,
+    genre: "Crime •",
+    caption: "No one gets away clean.",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: ted,
+    genre: "Comedy •",
+    caption: "Kindness makes a comeback",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: baseball,
+    genre: "",
+    caption: "Live MLB games, every Friday.",
+    buttonCaption: "See the schedule"
+  },
+
+  {
+    image: govCheese,
+    genre: "Comedy •",
+    caption: "A second chance is never simple.",
+    buttonCaption: "Stream now"
+  },
+
+  {
+    image: shrinking,
+    genre: "Comedy •",
+    caption: "Getting it together. Together",
+    buttonCaption: "Stream now"
+  }
+];
+
+export default slides
